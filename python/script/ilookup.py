9dcbe558cb179b3c23c3fd9d91d9d80ed3400d0c
davide_w <ywei@rosettastone.com>
2015-04-19 18:46:16 +0800

    add log and error handler

--- python/script/ilookup.py
+++ python/script/ilookup.py
@@ -1,6 +1,16 @@
 import requests
 import json
 import sys
+import logging
+
+logging.basicConfig(
+  #filename = 'log.log',
+  stream = sys.stdout,
+  level = logging.DEBUG,
+  format = '%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s:%(message)s', 
+  datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger()
 
 class Lookup(object):
 
@@ -13,14 +23,19 @@ class Lookup(object):
     pass
 
   def lookup(self, aid):
-    response = requests.get(self.url+aid, headers = self.headers)
-    ret = json.loads(response.text)
-    print len(ret['results'])
-    return len(ret['results']) , ret
+    lens, resp = 0, None
+    try:
+      response = requests.get(self.url+aid, headers = self.headers)
+      ret = json.loads(response.text)
+      lens, resp =  len(ret['results']) , ret
+    except Exception as e:
+      logger.error(unicode(e))
+    return lens, resp
 
 if __name__ == '__main__':
   print sys.argv
   if len(sys.argv) == 1:
     exit('no argument')
   lp = Lookup()
-  print lp.lookup(sys.argv[1])
+  a, b = lp.lookup(sys.argv[1])
+  logger.info(str(a) + str(b) )
